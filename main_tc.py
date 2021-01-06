1a775298008f6336efab805c17cdd59056c4609d
parnika31 <76988938+parnika31@users.noreply.github.com>
2021-01-06 23:57:42 +0530

    Update main_tc.py

--- main_tc.py
+++ main_tc.py
@@ -82,6 +82,9 @@ def run(config):
                                   for acsp in env.action_space])
     t = 0
     for ep_i in range(0, config.n_episodes, config.n_rollout_threads):
+        print("Episodes %i-%i of %i" % (ep_i + 1,
+                                        ep_i + 1 + config.n_rollout_threads,
+                                        config.n_episodes))
         obs = env.reset()
         maddpg.prep_rollouts(device='cpu')
 
